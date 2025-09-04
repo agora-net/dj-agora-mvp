aeca657d3d610737d78e7135e76c79d1ebb0a0ef
Ewan Jones <ewan@climacrux.com>
2025-09-04 21:57:08 +0200

    feat: define PROJECT_DIR and update template directory settings for improved organization

--- agora/settings.py
+++ agora/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_DIR = BASE_DIR / "agora"
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +58,9 @@ ROOT_URLCONF = "agora.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            PROJECT_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
