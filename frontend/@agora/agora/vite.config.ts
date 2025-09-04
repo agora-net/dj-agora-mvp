e3b97c2ae2ee4e07bed8be2aeb638a04f7a4886b
Ewan Jones <ewan@climacrux.com>
2025-09-04 22:13:20 +0200

    feat: integrate Tailwind CSS and update Vite configuration for custom build paths

--- frontend/@agora/agora/vite.config.ts
+++ frontend/@agora/agora/vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/static/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist",
     manifest: "manifest.json",
     rollupOptions: {
       input: {
-        main: "src/tailwind.css",
+        tailwind: "src/tailwind.css",
       },
     },
   },
