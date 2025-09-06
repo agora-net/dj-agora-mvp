02fb991657e31cad388739be0566859f1236fca9
Ewan Jones <ewan@climacrux.com>
2025-09-06 10:22:16 +0200

    feat: add URL pattern for signup status view to retrieve waiting list position by signup ID

--- agora/urls.py
+++ agora/urls.py
@@ -18,11 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from agora.apps.core.views import custom_404, home, signup
+from agora.apps.core.views import custom_404, home, signup, signup_status
 
 urlpatterns = [
     path("", home, name="home"),
     path("signup/", signup, name="signup"),
+    path("signup/<uuid:signup_id>/", signup_status, name="signup_status"),
     path("404/", custom_404, name="404"),
     path("admin/", admin.site.urls),
 ]
