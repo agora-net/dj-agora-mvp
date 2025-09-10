ef3a24322dbfe44b3f1c9aa4bec697db6d95fe7f
Ewan Jones <ewan@climacrux.com>
2025-09-10 14:51:17 +0200

    fix: correct typo in BASE_DIR assignment in settings.py

--- agora/settings.py
+++ agora/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parenti
+BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = BASE_DIR / "agora"
 
 env = environ.Env()
