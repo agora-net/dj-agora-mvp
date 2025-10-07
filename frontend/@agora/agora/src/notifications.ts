5f06d981f7e9c6fbe0cfa5930538c1509cba8507
Ewan Jones <ewan@climacrux.com>
2025-10-07 17:48:08 +0200

    feat: enhance showNotification function to support multiple tags with Tailwind CSS styling

--- frontend/@agora/agora/src/notifications.ts
+++ frontend/@agora/agora/src/notifications.ts
@@ -1,17 +1,30 @@
  
 import Toastify from 'toastify-js';
-import "toastify-js/src/toastify.css"
+import "./toastify.css"
 
 declare global {
     interface Window {
-        showNotification: (message: string, duration?: number) => void;
+        showNotification: (message: string, tags?: string, duration?: number) => void;
     }
 }
 
-export const showNotification = (message: string, duration = 3000) => {
+export const showNotification = (message: string, tags: string = "", duration = 3000) => {
+
+    const tagToTailwindClassMap: { [key: string]: string } = {
+        // Map Django message tags to Tailwind CSS classes
+        "debug": "bg-base-300 text-base-content",
+        "info": "bg-info text-info-content",
+        "success": "bg-success text-success-content",
+        "warning": "bg-warning text-warning-content",
+        "error": "bg-error text-error-content",
+    };
+
+    const className = tags.split(" ").map(tag => tagToTailwindClassMap[tag] || tag).join(" ");
+
     Toastify({
         text: message,
         duration,
+        className,
         close: true,
         stopOnFocus: true,
         gravity: "top", // `top` or `bottom`
