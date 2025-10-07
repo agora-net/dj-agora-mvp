eb5ee9cb2494a0ae84d3d2621d0bd2b8089d223d
Ewan Jones <ewan@climacrux.com>
2025-10-07 15:31:28 +0200

    feat: enhance Stripe identity verification session with user metadata and idempotency key

--- agora/apps/core/api_v1.py
+++ agora/apps/core/api_v1.py
@@ -5,6 +5,8 @@ from ninja.security import django_auth
 
 from agora import selectors
 
+from .models import AgoraUser
+
 # Use the fork of Django Ninja
 # https://pmdevita.github.io/django-shinobi/
 api = NinjaAPI(
@@ -23,11 +25,28 @@ class StripeIdentityResponse(Schema):
 
 @api.post("/identity/stripe/", auth=django_auth, response={201: StripeIdentityResponse})
 def create_stripe_identity_verification_session(request):
-    logger.info("creating Stripe identity verification session")
+    # we know the user is authenticated because of the auth decorator on the API
+    user: AgoraUser = request.user  # type: ignore
+
+    logger.info("creating Stripe identity verification session", user_id=user.id)
+
+    idempotency_key = selectors.stripe_idempotency_key_time_based(
+        prefix="create_stripe_identity_verification_session",
+        unique_key=request.session.session_key,
+    )
 
     verification_session = stripe.identity.VerificationSession.create(
         verification_flow=selectors.stripe_identity_verification_flow(request=request),
+        idempotency_key=idempotency_key,
+        metadata={
+            "user_id": str(user.id),
+            "keycloak_id": str(user.keycloak_id),
+        },
     )
 
-    logger.info("created Stripe identity verification session", id=verification_session.id)
+    logger.info(
+        "created Stripe identity verification session",
+        user_id=user.id,
+        id=verification_session.id,
+    )
     return (201, verification_session)
