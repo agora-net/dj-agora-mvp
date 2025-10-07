366d7b7c40d3507a0cb1beaf269aeb338e32c1e5
Ewan Jones <ewan@climacrux.com>
2025-10-07 15:00:23 +0200

    feat: enhance Stripe identity verification session with logging and flow integration

--- agora/apps/core/api_v1.py
+++ agora/apps/core/api_v1.py
@@ -1,6 +1,9 @@
 import stripe
+import structlog
 from ninja import NinjaAPI, Schema
 from ninja.security import django_auth
+
+from agora import selectors
 
 # Use the fork of Django Ninja
 # https://pmdevita.github.io/django-shinobi/
@@ -11,6 +14,8 @@ api = NinjaAPI(
     csrf=True,
 )
 
+logger = structlog.get_logger(__name__)
+
 
 class StripeIdentityResponse(Schema):
     client_secret: str
@@ -18,6 +23,11 @@ class StripeIdentityResponse(Schema):
 
 @api.post("/identity/stripe/", auth=django_auth, response={201: StripeIdentityResponse})
 def create_stripe_identity_verification_session(request):
-    verification_session = stripe.identity.VerificationSession.create()
+    logger.info("creating Stripe identity verification session")
 
+    verification_session = stripe.identity.VerificationSession.create(
+        verification_flow=selectors.stripe_identity_verification_flow(request=request),
+    )
+
+    logger.info("created Stripe identity verification session", id=verification_session.id)
     return (201, verification_session)
