ee0e2071944fb8b8df7698b0a33106d8a101cfe7
Ewan Jones <ewan@climacrux.com>
2025-10-07 14:01:35 +0200

    feat: implement stripe_identity_verification_flow function to retrieve flow ID

--- agora/selectors.py
+++ agora/selectors.py
@@ -1,5 +1,18 @@
+from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+from django.http import HttpRequest
 from django.urls import NoReverseMatch, reverse
+
+
+def stripe_identity_verification_flow(*, request: HttpRequest) -> str:
+    """
+    Get the Stripe identity verification flow ID for the current user.
+    Returns the flow ID string or None if not set.
+
+    Takes the request for future use so we can do things like A/B testing.
+    """
+
+    return settings.STRIPE_IDENTITY_VERIFICATION_FLOW
 
 
 def is_named_url(url_string):
