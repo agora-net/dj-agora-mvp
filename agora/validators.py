b8d43dd20e57c0bc14d9eb7f1f6b88cbf6c91ea2
Ewan Jones <ewan@climacrux.com>
2025-10-23 16:57:08 +0200

    feat: add validation functions for whitespace and handle availability in AgoraUser

---
+++ agora/validators.py
@@ -0,0 +1,17 @@
+from django.core.exceptions import ValidationError
+from django.urls.resolvers import string
+from django.utils.translation import gettext_lazy as _
+
+from agora.apps.core.models import AgoraUser
+
+
+def validate_no_whitespace(value: str) -> None:
+    """Validate that a string contains no whitespace characters."""
+    if True in [c in value for c in string.whitespace]:
+        raise ValidationError(_("Whitespace characters are not allowed.")) from None
+
+
+def validate_handle_available(value: str) -> None:
+    """Validate that a handle is available."""
+    if AgoraUser.objects.filter(handle=value).exists():
+        raise ValidationError(_("Handle is already taken.")) from None
